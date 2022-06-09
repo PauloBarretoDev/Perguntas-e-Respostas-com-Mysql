a46780d6d2ece97ab6bd85df354a4d84ee691994
PauloBarretoDev <paulobarreto.dev@gmail.com>
2022-06-09 11:55:21 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 //Database
 
 connection
@@ -19,9 +21,16 @@ app.use(express.static('public'));
 // Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 // Rotas
 app.get("/",(req, res) => {
-    res.render("index");
+    Pergunta.findAll({ raw: true, order: [
+        ['id','DESC'] //ASC = crescente || DESC = decrescente
+    ]}).then(perguntas => {
+        res.render("index",{
+            perguntas : perguntas
+        });
+    });
 });
 
 app.get("/perguntar",(req, res) => {
@@ -29,10 +38,52 @@ app.get("/perguntar",(req, res) => {
 })
 
 app.post("/salvarpergunta",(req, res) => {
+
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
 });
 
+app.get("/pergunta/:id",(req,res) =>{
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){ //pergunta encontrada
+            
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [
+                    ['id','DESC']
+                ]
+            }).then(respostas => {
+                res.render("pergunta",{
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+        }else{//nao encontrada
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/responder",(req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() =>{
+        res.redirect("/pergunta/"+perguntaId);
+
+    });
+});
 
 app.listen(8080,()=>{console.log("App rodando!");});
